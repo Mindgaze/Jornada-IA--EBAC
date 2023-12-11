adca4f43087bc9e063f1d32f230b359561ce0476
Filipe Do Nascimento Rodrigues <128511988+Mindgaze@users.noreply.github.com>
2023-12-11 16:42:02 -0300

    Delete utils.js
    Para upload da versão final

--- utils.js
+++
@@ -1,6 +0,0 @@
-///Função para medir a distancia entre dois pontos: Player e Obstaculo///
-export default{
-    getDistance(obj1,obj2){
-        return Math.sqrt(Math.abs(obj1.x-obj2.x)**2+Math.abs(obj1.y-obj2.y)**2);
-    }
-}
